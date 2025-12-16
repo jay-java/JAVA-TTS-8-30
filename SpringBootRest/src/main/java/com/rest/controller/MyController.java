2ca69e26f577e139c9b02fc4849c6fea8f453096
jay-java <jayprajapati.tops@gmail.com>
2025-12-16 09:36:53 +0530

    spring boot rest intro

--- SpringBootRest/src/main/java/com/rest/controller/MyController.java
+++ SpringBootRest/src/main/java/com/rest/controller/MyController.java
@@ -1,20 +1,23 @@
 package com.rest.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rest.model.User;
 
 @RestController
 public class MyController {
 
 	@GetMapping("/")
-	public String helloMsg() {
-		return "hello user";
+	public String msg() {
+		return "msg";
 	}
 
+	@GetMapping("/user")
+	public User usersData() {
+		User u = new User(1, "java", "ahmedabad", 987654321, "java@gmail.com", "java@123");
+		return u;
+	}
 }
---
+++ SpringBootRest/src/main/java/com/rest/model/User.java
@@ -0,0 +1,27 @@
+package com.rest.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String address;
+	private long contact;
+	private String email;
+	private String password;
+}
